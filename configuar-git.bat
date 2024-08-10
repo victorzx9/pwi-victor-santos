5acaf0a1548963448c3a72399daf0a7262a7dc2f
victorzx9 <victordsf2008@gmail.com>
2024-08-09 21:41:46 -0300

    Primeira Pagina WEB

---
+++ configuar-git.bat
@@ -0,0 +1,13 @@
+echo OFF
+
+echo --- Configurando usario GitHUB ---
+echo
+echo
+git config --global user.name "victorzx9"
+git config --global user.email victordsf2008@gmail.com
+echo ---mostrando a configuração atual do GitHub no computador
+git config -- list
+echo
+echo ---Fim ---
+
+pause
